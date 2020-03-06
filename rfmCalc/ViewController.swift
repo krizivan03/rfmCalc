f954e0bc716d4fce4158df83559ecff4d6132188
Christopher Canales <krizivan03@yahoo.com>
2020-03-05 23:40:04 -0700

    Update ViewController.swift

--- rfmCalc/ViewController.swift
+++ rfmCalc/ViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Christopher Canales. All rights reserved.
 //
 
-import UIKit
+import UIKit 
 
 class ViewController: UIViewController {
     @IBOutlet weak var heightInput: UITextField!
